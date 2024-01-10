73dcd957315f455d6862fd22c3448302b94654d3
Jak <jak@giveon.me>
2024-01-10 16:27:01 -0500

    Change Build Pattern

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -6,45 +6,11 @@ import pkg from './package.json' assert { type: 'json' }
 
 export default [
   {
-    input: 'config.ts',
+    input: 'index.ts',
     external: Object.keys(pkg.dependencies),
     output: [
-      { file: 'dist/config.cjs.js', format: 'cjs', sourcemap: true, exports: 'named' },
-      { file: 'dist/config.esm.js', format: 'es', sourcemap: true, exports: 'named' },
-    ],
-    plugins: [
-      typescript({
-        outputToFilesystem: true,
-        exclude: ['bin/**/*', 'tests/**/*'],
-      }),
-      resolve(),
-      commonjs(),
-      json(),
-    ],
-  },
-  {
-    input: 'env.ts',
-    external: Object.keys(pkg.dependencies),
-    output: [
-      { file: 'dist/env.cjs.js', format: 'cjs', sourcemap: true, exports: 'named' },
-      { file: 'dist/env.esm.js', format: 'es', sourcemap: true, exports: 'named' },
-    ],
-    plugins: [
-      typescript({
-        outputToFilesystem: true,
-        exclude: ['bin/**/*', 'tests/**/*'],
-      }),
-      resolve(),
-      commonjs(),
-      json(),
-    ],
-  },
-  {
-    input: 'errors.ts',
-    external: Object.keys(pkg.dependencies),
-    output: [
-      { file: 'dist/errors.cjs.js', format: 'cjs', sourcemap: true, exports: 'named' },
-      { file: 'dist/errors.esm.js', format: 'es', sourcemap: true, exports: 'named' },
+      { file: pkg.main, format: 'cjs', sourcemap: true, exports: 'named' },
+      { file: pkg.module, format: 'es', sourcemap: true, exports: 'named' },
     ],
     plugins: [
       typescript({
